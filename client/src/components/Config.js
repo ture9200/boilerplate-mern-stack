9ada3fa6154c46cfa4c05f80f41263f3e9725999
ture9200 <didtjddms09@naver.com>
2022-06-01 17:23:52 +0900

    .

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,9 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
+
+
+/*앞에 위의 url을 입력할 필요가 없다. */ 
